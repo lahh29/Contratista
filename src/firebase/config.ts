d9237883636503389b0dafdfe50e3473ab0dcc5a
Leonardo <capacitacionvinoqro@gmail.com>
2026-03-13 22:55:26 +0000

    Listo

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDCI86FTRzFLfUjg751KJD72OmiB7jxmN8",
+  authDomain: "contratistas-d30db.firebaseapp.com",
+  projectId: "contratistas-d30db",
+  storageBucket: "contratistas-d30db.firebasestorage.app",
+  messagingSenderId: "830635750377",
+  appId: "1:830635750377:web:5f5a8d4ae1f5132b745178"
+};
